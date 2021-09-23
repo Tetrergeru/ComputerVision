3cd55e8dc3a2fa97cf752592bf8d12c31b7be8ce
Tetrergeru <41305740+Tetrergeru@users.noreply.github.com>
2021-09-23 22:00:15 +0300

    Add spline

--- 01-brightness/Brightness/Menus/InteractiveColorCorrection.cs
+++ 01-brightness/Brightness/Menus/InteractiveColorCorrection.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -13,6 +14,8 @@ namespace GraphFunc.Menus
 
         private List<Point> _linearInterpolation = new List<Point>();
         private List<Point> _splineInterpolationPoints = new List<Point>();
+
+        private bool _linearActive = true;
 
         public InteractiveColorCorrection()
         {
@@ -30,32 +33,21 @@ namespace GraphFunc.Menus
 
             _colorImages[1].MouseClick += (sender, args) =>
             {
-                if (args.Button == MouseButtons.Right)
-                {
-                    _linearInterpolation = _linearInterpolation.Where(p => Distance(p, args.Location) > 5).ToList();
-                    DrawLinear(_form);
-                }
-                else
-                {
-                    _linearInterpolation = _linearInterpolation.Append(args.Location).OrderBy(p => p.X).ToList();
-                    DrawLinear(_form);
-                }
+                _linearActive = true;
+                _linearInterpolation = args.Button == MouseButtons.Right
+                    ? _linearInterpolation.Where(p => Distance(p, args.Location) > 20).ToList()
+                    : _linearInterpolation.Append(args.Location).OrderBy(p => p.X).ToList();
+                DrawLinear(_form);
             };
 
             _colorImages[2].MouseClick += (sender, args) =>
             {
-                if (args.Button == MouseButtons.Right)
-                {
-                    _splineInterpolationPoints =
-                        _splineInterpolationPoints.Where(p => Distance(p, args.Location) > 5).ToList();
-                    DrawLinear(_form);
-                }
-                else
-                {
-                    _splineInterpolationPoints =
-                        _splineInterpolationPoints.Append(args.Location).OrderBy(p => p.X).ToList();
-                    DrawSpline(_form);
-                }
+                _linearActive = false;
+                _splineInterpolationPoints = args.Button == MouseButtons.Right
+                    ? _splineInterpolationPoints.Where(p => Distance(p, args.Location) > 20).ToList()
+                    : _splineInterpolationPoints.Append(args.Location).OrderBy(p => p.X).ToList();
+
+                DrawSpline(_form);
             };
         }
 
@@ -97,59 +89,70 @@ namespace GraphFunc.Menus
             g.DrawLine(pen, p0, new Point(_colorImages[1].Width, 0));
 
             Func<double, double> func = LinearFunc;
-            _colorImages[0].Image = FastBitmap
-                .Select(form.image.Scale(_colorImages[1].Width, _colorImages[1].Height), color => Color.FromArgb(
-                        Program.ToByte(func(color.R / 256.0) * 256),
-                        Program.ToByte(func(color.G / 256.0) * 256),
-                        Program.ToByte(func(color.B / 256.0) * 256)
-                    )
-                );
+
+            if (_linearActive)
+                _colorImages[0].Image = FastBitmap
+                    .Select(form.image.Scale(_colorImages[1].Width, _colorImages[1].Height), color => Color.FromArgb(
+                            Program.ToByte(func(color.R / 256.0) * 256),
+                            Program.ToByte(func(color.G / 256.0) * 256),
+                            Program.ToByte(func(color.B / 256.0) * 256)
+                        )
+                    );
         }
 
         private void DrawSpline(Form form)
         {
+            Console.WriteLine($">>{_splineInterpolationPoints.Count}");
             _colorImages[2].Image = new Bitmap(_colorImages[2].Width, _colorImages[2].Height);
+            var points = new[] {new Point(0, _colorImages[2].Height)}
+                .Concat(_splineInterpolationPoints)
+                .Append(new Point(_colorImages[2].Width, 0))
+                .ToList();
+            var interpolation = MathNet.Numerics.Interpolate.CubicSpline(
+                points.Select(p => (double) p.X),
+                points.Select(p => (double) p.Y)
+            );
+
             var g = Graphics.FromImage(_colorImages[2].Image);
             var pen = new Pen(Color.Black, 2);
             g.Clear(Color.Orange);
-            var p0 = new Point(0, _colorImages[2].Height);
-            foreach (var p in _splineInterpolationPoints)
+
+            var point0 = new Point(0, (int) interpolation.Interpolate(0));
+            for (var i = 1; i < _colorImages[2].Image.Width; i++)
             {
-                g.DrawLine(pen, p0, p);
-                p0 = p;
+                var point1 = new Point(i, (int) interpolation.Interpolate(i));
+                g.DrawLine(pen, point0, point1);
+                point0 = point1;
             }
 
-            g.DrawLine(pen, p0, new Point(_colorImages[2].Width, 0));
+            if (!_linearActive)
+                _colorImages[0].Image = FastBitmap
+                    .Select(form.image.Scale(_colorImages[0].Width, _colorImages[0].Height), color => Color.FromArgb(
+                            Program.ToByte(256 - interpolation.Interpolate(color.R)),
+                            Program.ToByte(256 - interpolation.Interpolate(color.G)),
+                            Program.ToByte(256 - interpolation.Interpolate(color.B))
+                        )
+                    );
         }
 
         private double LinearFunc(double x)
         {
-            if (_linearInterpolation.Count == 0)
-                return x;
-            var idx = -1;
-            for (var i = 0; i < _linearInterpolation.Count; i++)
-            {
-                if (_linearInterpolation[i].X > x)
-                {
-                    idx = i;
-                    break;
-                }
-            }
+            var points = new[] {new Point(0, _colorImages[1].Height)}
+                .Concat(_linearInterpolation)
+                .Append(new Point(_colorImages[1].Width, 0))
+                .ToList();
+            var idx = points.FindIndex(p => p.X > x);
 
-            var inter =
-                idx == -1
-                    ? InterpolateLine(_linearInterpolation[_linearInterpolation.Count - 1],
-                        new Point(_colorImages[1].Width, 0), x * 256)
-                    : idx == 0
-                        ? InterpolateLine(new Point(0, _colorImages[1].Height),
-                            _linearInterpolation[0], x * 256)
-                        : InterpolateLine(_linearInterpolation[idx - 1],
-                            _linearInterpolation[idx], x * 256);
-            return 1 - inter / 256;
+            var inter = InterpolateLine(
+                points[idx - 1],
+                points[idx],
+                x * 256
+            );
+            return inter / 256;
         }
 
         private double InterpolateLine(Point a, Point b, double x)
-            => (double) x * ((double) b.Y - a.Y) / ((double) b.X - a.X);
+            => (double) x * ((double) b.Y - a.Y) / -((double) b.X - a.X);
 
         private static double Distance(Point a, Point b)
             => Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
