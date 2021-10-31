cae3f2f31bb6dd4649e7cce143b99e3db59cb12a
Tetrergeru <41305740+Tetrergeru@users.noreply.github.com>
2021-10-31 23:32:08 +0300

    Lab 4

--- lab-04-filtration/src/main.rs
+++ lab-04-filtration/src/main.rs
@@ -1,73 +1,168 @@
 use opencv::{
     core::{
-        absdiff, bitwise_and, bitwise_not, bitwise_or, no_array, Mat, Point, Rect, Scalar,
-        BORDER_CONSTANT, CV_8UC1,
+        abs, absdiff, add, bitwise_and, bitwise_not, bitwise_or, convert_scale_abs, min_max_loc,
+        min_max_loc_sparse, no_array, pow, subtract, Mat, Point, Rect, Scalar, BORDER_CONSTANT,
+        BORDER_DEFAULT, CV_16SC1, CV_32F, CV_8SC1, CV_8UC1,
     },
     highgui,
     imgcodecs::{self, IMREAD_GRAYSCALE},
     imgproc::{
-        circle, dilate, erode, flood_fill, morphology_default_border_value, threshold, FILLED,
-        THRESH_BINARY,
+        circle, dilate, erode, flood_fill, laplacian, median_blur, morphology_default_border_value,
+        sobel, threshold, FILLED, THRESH_TOZERO,
     },
     prelude::*,
     Result,
 };
 
+fn show(name: &str, mat: &Mat) -> Result<()> {
+    highgui::named_window(name, 0)?;
+    highgui::imshow(name, &{
+        let mut clone = mat.clone();
+        mat.convert_to(&mut clone, CV_8UC1, 1.0, 0.0)?;
+        clone
+    })?;
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
-    highgui::named_window("gears", 0)?;
-
-    let image_file = imgcodecs::imread("./Gears.png", IMREAD_GRAYSCALE)?;
-
-    let image_bin = {
-        let mut binary = image_file.clone();
-        threshold(&image_file, &mut binary, 100.0, 255.0, THRESH_BINARY)?;
-        binary
+    let image_file = {
+        let img = imgcodecs::imread("./skeleton.jpg", IMREAD_GRAYSCALE)?;
+        let mut clone = img.clone();
+        img.convert_to(&mut clone, CV_32F, 1.0, 0.0)?;
+        clone
     };
 
-    highgui::imshow("gears", &image_bin)?;
-    highgui::wait_key(-1)?;
+    show("image_file", &image_file)?;
 
-    let image_filled = {
-        let flooded = flooded_image(&image_bin, (0, 0), (255, 255, 255))?;
-        or_image(&image_bin, &not_image(&flooded)?)?
+    let image_laplacian = {
+        let mut clone = image_file.clone();
+        laplacian(&image_file, &mut clone, CV_32F, 3, 1.0, 0.0, BORDER_DEFAULT)?;
+        clone
     };
 
-    highgui::imshow("gears", &image_filled)?;
-    highgui::wait_key(-1)?;
+    let image_laplacian_scaled = &correction(&image_laplacian)?;
+    show("image_laplacian", &image_laplacian_scaled)?;
 
-    let image_diff = diff_image(&opening(&image_filled, 15)?, &image_filled)?;
+    let image_laplacian_sum = {
+        let mut clone = image_laplacian.clone();
+        add(
+            &image_laplacian,
+            &image_file,
+            &mut clone,
+            &no_array()?,
+            CV_32F,
+        )?;
+        clone
+    };
 
-    highgui::imshow("gears", &image_diff)?;
-    highgui::wait_key(-1)?;
+    show("image_laplacian_sum", &image_laplacian_sum)?;
 
-    let image_cleared_diff = closing(&image_diff, 3)?;
+    let image_sobel = {
+        let mut clone = image_file.clone();
+        let mut clone2 = image_file.clone();
+        sobel(
+            &image_file,
+            &mut clone,
+            CV_32F,
+            1,
+            0,
+            3,
+            1.0,
+            0.0,
+            BORDER_DEFAULT,
+        )?;
+        sobel(
+            &image_file,
+            &mut clone2,
+            CV_32F,
+            0,
+            1,
+            3,
+            1.0,
+            0.0,
+            BORDER_DEFAULT,
+        )?;
+        let mut clone3 = image_file.clone();
+        add(
+            &abs_image(&clone)?,
+            &abs_image(&clone2)?,
+            &mut clone3,
+            &no_array()?,
+            CV_32F,
+        )?;
+        correction(&abs_image(&clone3)?)?
+    };
 
-    highgui::imshow("gears", &image_cleared_diff)?;
-    highgui::wait_key(-1)?;
+    show("image_sobel", &image_sobel)?;
 
-    let image_ring = &dilated_image(&image_cleared_diff, 7, 2)?;
+    let image_median_sobel = {
+        let mut clone = image_sobel.clone();
+        median_blur(&image_sobel, &mut clone, 5)?;
+        clone
+    };
 
-    highgui::imshow("gears", &image_ring)?;
-    highgui::wait_key(-1)?;
+    show("image_median_sobel", &image_median_sobel)?;
 
-    let image_cleared_ring = &opening(&image_ring, 7)?;
+    let image_mask = {
+        image_median_sobel
+            .mul(&image_laplacian_sum, 1.0 / 256.0)?
+            .to_mat()?
+    };
 
-    highgui::imshow("gears", &image_cleared_ring)?;
-    highgui::wait_key(-1)?;
+    show("image_mask", &image_mask)?;
 
-    let dilated_diff = eroded_image(&dilated_image(&image_diff, 7, 3)?, 7)?;
-    let break_points = dilated_image(
-        &eroded_image(&diff_image(&image_cleared_ring, &dilated_diff)?, 7)?,
-        15,
-        3,
-    )?;
+    let image_mask_sum = {
+        let mut clone = image_laplacian.clone();
+        add(&image_mask, &image_file, &mut clone, &no_array()?, CV_8UC1)?;
+        clone
+    };
 
-    let result = or_image(&break_points, &image_cleared_ring)?;
+    show("image_mask_sum", &image_mask_sum)?;
 
-    highgui::imshow("gears", &result)?;
+    let img_mat_sum_pow = {
+        let mut image_mask_sum_f = image_mask_sum.clone();
+        image_mask_sum.convert_to(&mut image_mask_sum_f, CV_32F, 1.0, 0.0)?;
+
+        let mut clone = image_mask_sum.clone();
+        pow(&image_mask_sum_f, 0.5, &mut clone)?;
+
+        let mut clone2 = image_mask_sum.clone();
+        clone.convert_to(&mut clone2, CV_8UC1, 16.0, 0.0)?;
+        clone2
+    };
+
+    show("img_mat_sum_pow", &img_mat_sum_pow)?;
+
     highgui::wait_key(-1)?;
 
     Ok(())
+}
+
+fn abs_image(image: &Mat) -> Result<Mat> {
+    abs(image)?.to_mat()
+}
+
+fn correction(image: &Mat) -> Result<Mat> {
+    let mut min = 0.0;
+    let mut max = 0.0;
+    let mut min_idx = Point::new(0, 0);
+    let mut max_idx = Point::new(0, 0);
+    min_max_loc(
+        image,
+        &mut min,
+        &mut max,
+        &mut min_idx,
+        &mut max_idx,
+        &no_array()?,
+    )?;
+    println!("{}, {}", min, max);
+    let mut clone = image.clone();
+    image.convert_to(&mut clone, CV_32F, 1.0, -min)?;
+
+    let mut clone2 = clone.clone();
+    clone.convert_to(&mut clone2, CV_32F, 256.0 / (max - min), 0.0)?;
+    Ok(clone2)
 }
 
 fn flooded_image(image: &Mat, seed: (i32, i32), color: (u8, u8, u8)) -> Result<Mat> {
