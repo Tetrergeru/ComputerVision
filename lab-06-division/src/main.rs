98f90857ddd61428a26413f242c08b22870253ed
Tetrergeru <41305740+Tetrergeru@users.noreply.github.com>
2021-11-28 18:56:24 +0300

    Fix lab 6

--- lab-06-division/src/main.rs
+++ lab-06-division/src/main.rs
@@ -1,12 +1,20 @@
+use std::ops::Sub;
+
 use opencv::{
     core::{
-        abs, add, dft, idft, log, magnitude, merge, min_max_loc, no_array, split, Mat, Point, Rect,
-        Scalar, VecN, BORDER_DEFAULT, CV_32F, CV_32SC1, CV_8UC1, CV_8UC3, CV_8UC4,
-        DFT_COMPLEX_INPUT, DFT_COMPLEX_OUTPUT, DFT_REAL_OUTPUT, DFT_SCALE,
+        abs, add, dft, idft, log, magnitude, merge, min_max_loc, no_array, normalize, split,
+        subtract, Mat, Point, Rect, Scalar, VecN, BORDER_DEFAULT, CV_32F, CV_32FC1, CV_32S,
+        CV_32SC1, CV_64F, CV_64FC1, CV_8UC1, CV_8UC3, CV_8UC4, DFT_COMPLEX_INPUT,
+        DFT_COMPLEX_OUTPUT, DFT_REAL_OUTPUT, DFT_SCALE, NORM_MINMAX,
     },
     highgui,
     imgcodecs::{self, IMREAD_COLOR, IMREAD_GRAYSCALE},
-    imgproc::{filter_2d, find_contours, flood_fill, sobel, threshold, watershed, THRESH_BINARY},
+    imgproc::{
+        circle, cvt_color, dilate, distance_transform, draw_contours, filter_2d, find_contours,
+        flood_fill, morphology_default_border_value, sobel, threshold, watershed,
+        CHAIN_APPROX_SIMPLE, COLOR_BGR2GRAY, COLOR_GRAY2BGR, COLOR_GRAY2RGB, DIST_L2, INTER_MAX,
+        LINE_8, RETR_EXTERNAL, THRESH_BINARY, THRESH_OTSU,
+    },
     prelude::*,
     types::VectorOfMat,
     Result,
@@ -24,78 +32,173 @@ fn show(name: &str, mat: &Mat) -> Result<()> {
 }
 
 fn main() -> Result<()> {
-    let raw_image = imgcodecs::imread("./src.png", IMREAD_COLOR)?;
     let image_file = {
         let img = imgcodecs::imread("./src.png", IMREAD_GRAYSCALE)?;
-        let mut clone = img.clone();
-        img.convert_to(&mut clone, CV_32F, 1.0 / 255.0, 0.0)?;
+        let mut clone = new_mat(CV_32F);
+        img.convert_to(&mut clone, CV_32F, 1.0 / 256.0, 0.0)?;
         clone
     };
 
     show("image file", &image_file)?;
-    let image_sobel = {
-        let mut kernel = Mat::zeros(3, 3, CV_32F)?.to_mat()?;
-        for i in 0..kernel.rows() {
-            for j in 0..kernel.cols() {
-                if i == 1 && j == 1 {
-                    continue;
-                }
-                *(kernel.at_2d_mut::<f32>(i, j)?) = 1.0;
-            }
-        }
-        *(kernel.at_2d_mut::<f32>(1, 1)?) = -8.0;
-        apply(&image_file, |from, to| {
-            filter_2d(
-                from,
-                to,
-                CV_32F,
-                &kernel,
-                Point::new(-1, -1),
-                0.0,
-                BORDER_DEFAULT,
-            )
-        })?
-    };
 
-    show("image_sobel", &image_sobel)?;
+    let kernel = kernel()?;
 
-    let mut markers = {
-        // find_contours(dist_8u, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-        let mut out = new_mat();
-        threshold(&image_file, &mut out, 0.4, 1.0, THRESH_BINARY)?;
-        apply(&out, |from, to| from.convert_to(to, CV_32SC1, 1.0, 0.0))?
-    };
-    show("markers", &markers)?;
-
-    println!(
-        "src.type = {}, dst.type =  {} (CV_32F = {}, CV_8UC3 = {})",
-        raw_image.typ()?,
-        markers.typ()?,
+    let mut image_laplacian = new_mat(CV_32F);
+    filter_2d(
+        &image_file,
+        &mut image_laplacian,
         CV_32F,
-        CV_8UC3
-    );
-    watershed(&raw_image, &mut markers)?;
+        &kernel,
+        Point::new(-1, -1),
+        0.0,
+        BORDER_DEFAULT,
+    )?;
 
-    let mut dst = Mat::zeros(markers.rows(), markers.cols(), CV_8UC4)?.to_mat()?;
+    let image_result = sub_image(&image_file, &image_laplacian)?;
 
-    for i in 0..markers.rows() {
-        for j in 0..markers.cols() {
-            let &index = markers.at_2d::<i32>(i, j)?;
-            if index > 0 {
-                unsafe {
-                    *(dst.at_2d::<VecN<u8, 4>>(i, j)? as *const VecN<u8, 4> as usize
-                        as *mut VecN<u8, 4>) = VecN::<u8, 4>::all(255);
-                }
-            }
-        }
+    let image_result_cvt = convert(&image_result, CV_8UC1, 255.0)?;
+    let image_laplacian = convert_color(&image_laplacian, COLOR_GRAY2RGB)?;
+
+    highgui::imshow("image_laplacian", &image_laplacian)?;
+    highgui::imshow("image_result", &image_result_cvt)?;
+
+    println!("CV_8UC3 {}", CV_8UC3);
+    println!("CV_32F {}", CV_32F);
+    println!("CV_32S {}", CV_32S);
+
+    let mut bw_thr = new_mat(CV_32F);
+    threshold(
+        &image_result_cvt,
+        &mut bw_thr,
+        150.0,
+        255.0,
+        THRESH_BINARY | THRESH_OTSU,
+    )?;
+    highgui::imshow("Binary Image", &bw_thr)?;
+
+    let mut dist = new_mat(CV_32F);
+
+    distance_transform(&bw_thr, &mut dist, DIST_L2, 3, CV_32F)?;
+
+    let mut dist_norm = new_mat(CV_32F);
+    normalize(
+        &dist,
+        &mut dist_norm,
+        0.0,
+        1.0,
+        NORM_MINMAX,
+        -1,
+        &no_array(),
+    )?;
+    highgui::imshow("Distance Transform Image", &dist_norm)?;
+
+    let mut dist_norm_thr = new_mat(CV_32F);
+    threshold(&dist_norm, &mut dist_norm_thr, 0.05, 1.0, THRESH_BINARY)?;
+
+    let mut dist_norm_thr_dilated = new_mat(CV_32F);
+    let kernel1 = Mat::ones(5, 5, CV_8UC1)?;
+    dilate(
+        &dist_norm_thr,
+        &mut dist_norm_thr_dilated,
+        &kernel1,
+        Point::new(-1, -1),
+        4,
+        BORDER_DEFAULT,
+        morphology_default_border_value()?,
+    )?;
+
+    let dist_norm_thr_dilated = invert(&dist_norm_thr_dilated)?; // ***
+    highgui::imshow("Peaks", &dist_norm_thr_dilated)?;
+
+    let dist_8u = convert(&dist_norm_thr_dilated, CV_8UC1, 1.0)?;
+    let mut contours = VectorOfMat::new();
+    find_contours(
+        &dist_8u,
+        &mut contours,
+        RETR_EXTERNAL,
+        CHAIN_APPROX_SIMPLE,
+        Point::new(0, 0),
+    )?;
+
+    let mut markers = Mat::zeros(dist.rows(), dist.cols(), CV_32S)?.to_mat()?;
+    println!("contours.len(): {}", contours.len());
+    for i in 0..contours.len() {
+        draw_contours(
+            &mut markers,
+            &contours,
+            i as i32,
+            Scalar::all(i as f64 + 1.0),
+            -1,
+            LINE_8,
+            &no_array(),
+            i32::MAX,
+            Point::new(0, 0),
+        )?;
+    }
+    for (x, y) in [(5, 5), (72, 76), (136, 108), (20, 156), (154, 22), (66, 109)] {
+        circle(
+            &mut markers,
+            Point::new(x, y),
+            3,
+            Scalar::all(255.0),
+            -1,
+            LINE_8,
+            0,
+        )?;
     }
 
-    highgui::named_window("watershed", 0)?;
-    highgui::imshow("watershed", &dst)?;
+    let markers_8u = convert(&markers, CV_8UC1, 20.0)?;
+    highgui::imshow("Markers", &markers_8u)?;
+
+    let image_result = convert(&convert_color(&image_file, COLOR_GRAY2BGR)?, CV_8UC3, 1.0)?;
+    println!("type {}", image_result.typ());
+
+    watershed(&image_result, &mut markers)?;
+
+    let mark = convert(&markers, CV_8UC1, 20.0)?;
+    highgui::imshow("watershed", &mark)?;
 
     highgui::wait_key(-1)?;
 
     Ok(())
+}
+
+fn kernel() -> Result<Mat> {
+    let mut mat = Mat::zeros(3, 3, CV_64FC1)?.to_mat()?;
+
+    let data = [[1.0, 1.0, 1.0], [1.0, -8.0, 1.0], [1.0, 1.0, 1.0]];
+
+    for i in 0..3 {
+        for j in 0..3 {
+            *mat.at_2d_mut(i, j)? = data[i as usize][j as usize];
+        }
+    }
+
+    Ok(mat)
+}
+
+fn invert(mat: &Mat) -> Result<Mat> {
+    let mut clone = new_mat(CV_32FC1);
+    mat.convert_to(&mut clone, CV_32FC1, -1.0, 1.0)?;
+    Ok(clone)
+}
+
+fn sub_image(left: &Mat, right: &Mat) -> Result<Mat> {
+    let mut clone = left.clone();
+    subtract(&left, &right, &mut clone, &no_array(), -1)?;
+    Ok(clone)
+}
+
+fn convert_color(img: &Mat, cvt: i32) -> Result<Mat> {
+    let mut clone = img.clone();
+    cvt_color(&img, &mut clone, cvt, 0)?;
+    Ok(clone)
+}
+
+fn convert(mat: &Mat, to: i32, alpha: f64) -> Result<Mat> {
+    let mut clone = new_mat(to);
+    mat.convert_to(&mut clone, to, alpha, 0.0)?;
+    Ok(clone)
 }
 
 fn apply<F: Fn(&Mat, &mut Mat) -> Result<()>>(src: &Mat, f: F) -> Result<Mat> {
@@ -104,8 +207,8 @@ fn apply<F: Fn(&Mat, &mut Mat) -> Result<()>>(src: &Mat, f: F) -> Result<Mat> {
     Ok(clone)
 }
 
-fn new_mat() -> Mat {
-    Mat::zeros(0, 0, CV_32F).unwrap().to_mat().unwrap()
+fn new_mat(typ: i32) -> Mat {
+    Mat::zeros(0, 0, typ).unwrap().to_mat().unwrap()
 }
 
 fn abs_image(image: &Mat) -> Result<Mat> {
@@ -121,7 +224,7 @@ fn correction(image: &Mat) -> Result<Mat> {
         Some(&mut max),
         None,
         None,
-        &no_array()?,
+        &no_array(),
     )?;
     let mut clone = image.clone();
     image.convert_to(&mut clone, CV_32F, 1.0, -min)?;
